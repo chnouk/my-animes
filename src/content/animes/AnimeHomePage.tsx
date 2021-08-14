8ee27a77357562c095a85170f5a0bdef636435ba
chnouk <chnouk@users.noreply.github.com>
2021-08-14 23:34:13 +0200

    initial animes home page

--- src/content/animes/AnimeHomePage.tsx
+++ src/content/animes/AnimeHomePage.tsx
@@ -1,10 +1,35 @@
-import { Box } from '@material-ui/core';
+import { Box, Grid, InputAdornment, TextField } from '@material-ui/core';
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
 
 const AnimeHomePage = () => {
 
     return (
-        <Box>AnimeHomePage</Box>
+    <Grid container spacing={2}>
+
+        <Grid item xs={12} />
+
+        <Grid item xs={2} />
+        <Grid item xs={8}>
+        <TextField
+            fullWidth
+            variant='outlined'
+            label="Search"
+            placeholder="Search animes"
+            InputProps={{
+                startAdornment: (
+                    <InputAdornment position="start">
+                        <SearchIcon />
+                    </InputAdornment>
+                ),
+            }}
+        />
+        </Grid>
+        <Grid item xs={2} />
+
+        <Grid item xs={12} /> 
+
+    </Grid>
     )
 
 }
